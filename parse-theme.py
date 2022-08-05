0ce365d4f422e40c5ac6ab3ac465033f9547c99b
smathot <s.mathot@cogsci.nl>
2022-08-05 19:38:30 +0200

    Update theme

--- parse-theme.py
+++ parse-theme.py
@@ -5,24 +5,24 @@ import os
 import yaml
 
 with open('constants.yaml') as f:
-	const = yaml.load(f)
+    const = yaml.load(f, Loader=yaml.SafeLoader)
 
 
 def parse_folder(dirname):
 
-	for basename in os.listdir(dirname):
-		path = os.path.join(dirname, basename)
-		if os.path.isdir(path):
-			parse_folder(path)
-			continue
-		if path.endswith('.js'):
-			print('processing %s' % path)
-			with open(path) as fd:
-				content = fd.read()
-			with open(path, 'w') as fd:
-				for var, val in const.items():
-					content = content.replace(u'$%s$' % var, str(val))
-				fd.write(content)
+    for basename in os.listdir(dirname):
+        path = os.path.join(dirname, basename)
+        if os.path.isdir(path):
+            parse_folder(path)
+            continue
+        if path.endswith('.js'):
+            print('processing %s' % path)
+            with open(path) as fd:
+                content = fd.read()
+            with open(path, 'w') as fd:
+                for var, val in const.items():
+                    content = content.replace(u'$%s$' % var, str(val))
+                fd.write(content)
 
 
 parse_folder('output')
